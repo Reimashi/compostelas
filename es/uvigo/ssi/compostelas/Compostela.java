9c4300360bdd5d4facc17a0d25bbc2900c45febc
Aitor González Fernández <reimashi@gmail.com>
2014-10-18 22:32:47 +0200

    Ahora la compostela se puede guardar y cargar desde un XML

--- es/uvigo/ssi/compostelas/Compostela.java
+++ es/uvigo/ssi/compostelas/Compostela.java
@@ -1,8 +1,18 @@
 package es.uvigo.ssi.compostelas;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Compostela implements Serializable {
     private Pilgrim pilgrim;
@@ -14,7 +24,50 @@ public class Compostela implements Serializable {
         this.pilgrim = p;
     }
     
-    private void AddStamp (Stamp s) {
+    public void AddStamp (Stamp s) {
         this.stamps.add(s);
+    }
+    
+    public Pilgrim getPilgrim() {
+        return this.pilgrim;
+    }
+    
+    public List<Stamp> getStamps() {
+        return this.stamps;
+    }
+    
+    public void SaveFile (String path) throws FileNotFoundException, IOException {
+        try (FileOutputStream fos = new FileOutputStream(path);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                XMLEncoder encoder = new XMLEncoder(bos)) {
+            encoder.writeObject(this);
+        } 
+        catch (FileNotFoundException ex) {
+            Logger.getLogger(Compostela.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(Compostela.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+    
+    public static Compostela loadFromFile (String path) throws FileNotFoundException, IOException {
+        Compostela toret = null;
+        
+        try (FileInputStream fis = new FileInputStream(path);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                XMLDecoder decoder = new XMLDecoder(bis)) {
+            toret = (Compostela) decoder.readObject();
+        } 
+        catch (FileNotFoundException ex) {
+            Logger.getLogger(Compostela.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        } 
+        catch (IOException ex) {
+            Logger.getLogger(Compostela.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+        return toret;
     }
 }
