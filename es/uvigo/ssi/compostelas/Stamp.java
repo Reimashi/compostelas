fcc69b6ef840f5429bec396f90b0ddfdba068491
Aitor González Fernández <reimashi@gmail.com>
2014-10-20 12:21:53 +0200

    Añadidas funciones restantes

--- es/uvigo/ssi/compostelas/Stamp.java
+++ es/uvigo/ssi/compostelas/Stamp.java
@@ -2,7 +2,27 @@ package es.uvigo.ssi.compostelas;
 
 import java.io.Serializable;
 
+/**
+ * Sello digital de una compostela
+ */
 public class Stamp implements Serializable {
     private String owner;
     private byte[] sign;
+    
+    private Stamp() {}
+    
+    public Stamp (Signer signer, Pilgrim pil) {
+        this.owner = signer.getName();
+        this.sign = this.calculateSign(signer, pil);
+    }
+    
+    /**
+     * Calcula la firma digital de la compostela para un signer (albergue). A partir del hash?
+     * @param signer Usuario que firma la compostela
+     * @param pil Información del peregrino
+     * @return Firma digital
+     */
+    private byte[] calculateSign(Signer signer, Pilgrim pil) {
+        throw new UnsupportedOperationException();
+    }
 }
