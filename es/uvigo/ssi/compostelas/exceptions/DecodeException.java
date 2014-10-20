583c7e31bf190120a50fd1ca8cc2346313823e75
Aitor González Fernández <reimashi@gmail.com>
2014-10-20 23:49:05 +0200

    Excepciones generales de errores de codificación

---
+++ es/uvigo/ssi/compostelas/exceptions/DecodeException.java
@@ -0,0 +1,7 @@
+package es.uvigo.ssi.compostelas.exceptions;
+
+public class DecodeException extends Exception {
+    public DecodeException(String message) {
+        super(message);
+    }
+}
---
+++ es/uvigo/ssi/compostelas/exceptions/EncodeException.java
@@ -0,0 +1,7 @@
+package es.uvigo.ssi.compostelas.exceptions;
+
+public class EncodeException extends Exception {
+    public EncodeException(String message) {
+        super(message);
+    }
+}
