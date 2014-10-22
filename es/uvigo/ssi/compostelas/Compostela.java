c46abbef894ca44b98a6ddb468fac3d709ea00b8
Aitor González Fernández <reimashi@gmail.com>
2014-10-22 17:09:08 +0200

    Refactorizando código

--- es/uvigo/ssi/compostelas/Compostela.java
+++ es/uvigo/ssi/compostelas/Compostela.java
@@ -2,13 +2,7 @@ package es.uvigo.ssi.compostelas;
 
 import es.uvigo.ssi.compostelas.exceptions.DecodeException;
 import es.uvigo.ssi.compostelas.exceptions.EncodeException;
-import java.beans.XMLDecoder;
-import java.beans.XMLEncoder;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
