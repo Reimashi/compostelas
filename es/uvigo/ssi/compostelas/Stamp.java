fe7bfb6ddd878823871889fcdc995c7601d6d735
Aitor González Fernández <reimashi@gmail.com>
2014-10-22 19:41:52 +0200

    Añadidos los metodos de verificación de sellos

--- es/uvigo/ssi/compostelas/Stamp.java
+++ es/uvigo/ssi/compostelas/Stamp.java
@@ -2,6 +2,7 @@ package es.uvigo.ssi.compostelas;
 
 import es.uvigo.ssi.compostelas.exceptions.DecodeException;
 import es.uvigo.ssi.compostelas.exceptions.EncodeException;
+import java.io.FileNotFoundException;
 import java.io.Serializable;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -19,6 +20,8 @@ import org.json.simple.JSONObject;
  * Sello digital de una compostela
  */
 public class Stamp implements Serializable {
+    private static final Logger _log = Logger.getLogger(Stamp.class.getName());
+    
     private final String owner;
     private final byte[] sign;
     
@@ -29,18 +32,44 @@ public class Stamp implements Serializable {
     
     public Stamp (Signer signer, PilgrimEncoded pil) throws DecodeException, EncodeException {
         this.owner = signer.getName();
-        this.sign = this.calculateSign(signer, pil);
+        this.sign = this.calculateSign(signer, pil.getSign());
     }
     
+    /**
+     * Comprueba la firma digital de un sello.
+     * @param pil Información del peregrino.
+     * @return true si es correcta; false si es erronea.
+     * @throws DecodeException
+     * @throws EncodeException 
+     */
+    public boolean checkStamp(PilgrimEncoded pil) throws DecodeException, EncodeException {
+        try {
+            Signer stampsig = Signer.loadFromFile(this.owner);
+            return (this.sign == this.calculateSign(stampsig, pil.getSign()));
+        } catch (FileNotFoundException ex) {
+            Stamp._log.log(Level.SEVERE, null, ex);
+            throw new DecodeException("Error while open signer key file to chech a Stamp. " + ex.getMessage());
+        }
+    }
+    
+    /**
+     * Serializa Stamp en un objeto JSON.
+     * @return Objeto Stamp serializado en JSON.
+     */
     public JSONObject toJSON() {
         JSONObject jsonObj = new JSONObject();
         
         jsonObj.put("signer", this.owner);
-        jsonObj.put("sign", Base64.getEncoder().encode(this.sign));
+        jsonObj.put("sign", Base64.getEncoder().encodeToString(sign));
         
         return jsonObj;
     }
     
+    /**
+     * Obtiene un objeto de tipo Stamp desde un objeto JSON.
+     * @param joc Objeto Stamp serializado en JSON.
+     * @return Objeto Stamp des-serializado.
+     */
     public static Stamp fromJSON (JSONObject joc) {
         return new Stamp((String) joc.get("signer"), 
                 Base64.getDecoder().decode((String) joc.get("sign")));
@@ -49,13 +78,11 @@ public class Stamp implements Serializable {
     /**
      * Calcula la firma digital de la compostela para un signer (albergue). A partir del hash?
      * @param signer Usuario que firma la compostela
-     * @param pil Información del peregrino
+     * @param hash Datos a firmar
      * @return Firma digital
      */
-    private byte[] calculateSign(Signer signer, PilgrimEncoded pil) throws DecodeException, EncodeException {
+    private byte[] calculateSign(Signer signer, byte[] hash) throws DecodeException, EncodeException {
         try {
-            byte[] hash = PilgrimEncoded.getSHA1Hash(pil);
-            
             Cipher cifrador = Cipher.getInstance("RSA", "BC");
             cifrador.init(Cipher.ENCRYPT_MODE, signer.getKey().getPrivate());
             return cifrador.doFinal(hash);
